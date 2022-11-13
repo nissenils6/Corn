f00d4e541a03b30822843231d0af178b49a56ba9
nisse <nissenils6@gmail.com>
2022-11-13 17:56:58 +0100

    Commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,51 +7,27 @@ import syn.{GlobalStmt, ParserState, parseGlobalStmts}
 import java.io.PrintWriter
 import scala.languageFeature.implicitConversions
 
-private val ON_DEKSTOP = false
+private val ON_DEKSTOP = true
 
 private val PATH = if ON_DEKSTOP then "C:/Users/nisse/OneDrive/Skrivbord/Corn/TestCode" else "C:/Users/nisse/Desktop/Corn/TestCode"
+
 private val SOURCE_PATH = PATH + ".txt"
 private val ASSEMBLY_PATH = PATH + ".asm"
 
-def parseFile(parserState: ParserState): List[GlobalStmt] = parseGlobalStmts(List(), parserState)._1.reverse
-
-def compile(filePath: String): Unit = {
-  def printSeparator(): Unit = {
-    println()
-    println("-" * 128)
-    println()
+@main def main(): Unit = try {
+  val file = File(SOURCE_PATH)
+  val tokens = tokenize2(file)
+  val parsedFile = parseGlobalStmts(List(), ParserState(tokens, file))._1.reverse
+  val module = analyzeFile(parsedFile, file)
+  new PrintWriter(ASSEMBLY_PATH) {
+    write(AsmGen.toString)
+    close()
   }
 
-  try {
-    val file = File(SOURCE_PATH)
-    val tokens = tokenize2(file)
-
-    printSeparator()
-
-    println(tokens.mkString(" "))
-
-//    val parsedFile = parseFile(parserState)
-//    printSeparator()
-//    println(parsedFile.mkString("\n\n"))
-//
-//    val module = analyzeFile(parsedFile, parserState.file)
-//    printSeparator()
-//    print(module.format(0))
-//
-//    printSeparator()
-//
-//    new PrintWriter(ASSEMBLY_PATH) {
-//      write(AsmGen.toString)
-//      close()
-//    }
-  } catch {
-    case error: (Error | ErrorGroup) =>
-      printSeparator()
-      print(error)
-      error.printStackTrace()
-  }
-}
-
-@main def main(): Unit = {
-  compile(SOURCE_PATH)
+  println(List(tokens.mkString(" "), parsedFile.mkString("\n\n"), module.format(0)).mkString("-" * 128 + "\n\n", "\n" + "-" * 128 + "\n\n", "\n" + "-" * 128))
+} catch {
+  case error: (Error | ErrorGroup) =>
+    error.printStackTrace()
+    print("\n" * 8)
+    print(error)
 }
